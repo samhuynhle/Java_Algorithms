544febfb51fb1b85d1e82e02600483e2b9674e99
samhuynhle <samhuynhle@gmail.com>
2019-11-20 12:56:43 -0800

    added comments for search in bst recursive method

--- search_in_BST/solution.java
+++ search_in_BST/solution.java
@@ -7,15 +7,15 @@ class Algorithm {
     }
 
     public TreeNode searchBST(TreeNode root, int val) { // Recursive method
-        if(root == null){
+        if(root == null){ // base case to return null if the node we get is null
             return null;
         }
-        if(root.val == val){
+        if(root.val == val){ // base case to return the current node if the val matches
             return root;
-        }else if(val < root.val){
+        }else if(val < root.val){ // this conditional checks for the left tree, use BST's properties
             return searchBST(root.left, val);
         }else{
-            return searchBST(root.right, val);
+            return searchBST(root.right, val); // this conditional checks for the right tree, use BST's properties
         }
     }
 
