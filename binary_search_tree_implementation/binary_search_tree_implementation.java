63a69ca3cca0f71149ee46dc1cf8e8026ceb10bd
samhuynhle <samhuynhle@gmail.com>
2019-12-14 02:05:50 -0800

    added delete moethod for bst implementation

--- binary_search_tree_implementation/binary_search_tree_implementation.java
+++ binary_search_tree_implementation/binary_search_tree_implementation.java
@@ -58,4 +58,40 @@ class BinarySearchTree {
             inorderRec(root.right);
         }
     }
+
+    // Delete Method
+    void deleteKey(int key){
+        root = deleteRec(root, key);
+    }
+
+    Node deleteRec(Node root, int key){
+        if(root == null){
+            return root;
+        }
+        if(key < root.key){
+            root.left = deleteRec(root.left, key);
+        } else if(key > root.key){
+            root.right = deleteRec(root.right, key);
+        } else {
+            if(root.left == null){
+                return root.right;
+            } else if(root.right == null){
+                return root.left;
+            }
+            
+            root.key = minValue(root.right);
+            root.right = deleteRec(root.right, root.key);
+        }
+
+        return root;
+    }
+
+    int minValue(Node root){
+        int minv = root.key;
+        while(root.left != null){
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
 }
