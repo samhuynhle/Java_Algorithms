510d69ceec0d475b3b0b8321e55187f69aa77475
unknown <samhuynhle@gmail.com>
2019-12-22 20:20:22 -0800

    reviewed Search In Binary Search Tree, left comments on how the algorithm is operating.

--- Search_In_Binary_Search_Tree/solution.java
+++ Search_In_Binary_Search_Tree/solution.java
@@ -1,27 +1,40 @@
+// We are going to demonstrate both the recursive and iterative ways in searching within a
+// Binary Search Tree. The primary thought is to look at the current node, left child, then right child.
+// This is a form of pre-order traversal (one of the Depth Frist Search Approaches), but we cut off
+// path depending on the target values relation to the current nodes' value
+
 class Algorithm {
-    public class TreeNode { // This is how the TreeNode class is set up
+    // This is the set up for the Binary Search Tree (assume all BST properties applied for input)
+    public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
         TreeNode(int x) {val = x;}
     }
 
-    public TreeNode searchBST(TreeNode root, int val) { // Recursive method
-        if(root == null){ // base case to return null if the node we get is null
+    // Recursive Method
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        // Base case: If the root is null we return null
+        if(root == null){
             return null;
         }
-        if(root.val == val){ // base case to return the current node if the val matches
+
+        // Base case: If the root's value equals our target value, we return the current root.
+        if(root.val == val){
             return root;
-        }else if(val < root.val){ // this conditional checks for the left tree, use BST's properties
+        // If we are not at the correct node, we check the left OR right, depending on the value's relation to current root's value
+        }else if(val < root.val){ 
             return searchBST(root.left, val);
         }else{
-            return searchBST(root.right, val); // this conditional checks for the right tree, use BST's properties
+            return searchBST(root.right, val);
         }
     }
 
-    public TreeNode searchBSTIterative(TreeNode root, int val) { // Iterative method
+    // Iterative Method
+    public TreeNode searchBSTIterative(TreeNode root, int val) {
         while(root != null && root.val != val){ // This is how we are traversing through the tree
-            root = val < root.val ? root.left:root.right; // Ternary
+            root = val < root.val ? root.left:root.right;
         }
         return root; 
     }
